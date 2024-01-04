c9a3f5b85bc26c92f27bcd7e2af781cb5f85be6c
sikwan <yysskk99999@naver.com>
2024-01-04 19:39:39 +0900

    NaverCustomClone first

---
+++ NaverCustomClone/NaverCustomClone/NaverCustomCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverCustomCloneApp.swift
+//  NaverCustomClone
+//
+//  Created by 양시관 on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NaverCustomCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
